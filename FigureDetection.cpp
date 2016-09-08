f9a53aa09b8e419b42588212a9874d7b0778b2f0
gsdsgm <gsdsgm@gmail.com>
2016-09-08 20:02:33 +0200

    Change to new HSV color seperation method Fixed crash if no contour was found

--- FigureDetection.cpp
+++ FigureDetection.cpp
@@ -13,7 +13,7 @@ using namespace cv;
 namespace mt
 {
 
-uint8_t locateFigure(figureDescription& desc, Mat& srcimage, Rect& rect)
+uint8_t locateFigure(const colorDescription& desc, const Mat& srcimage, Rect& rect)
 {
 	// Assume the srcimage is already in HSV color space
 	Mat dstimage;
@@ -22,7 +22,7 @@ uint8_t locateFigure(figureDescription& desc, Mat& srcimage, Rect& rect)
 	uint32_t largest_contour_index = 0;
 	uint32_t largest_area = 0;
 
-	inRange(srcimage, desc.lowerThreshold, desc.upperThreshold, dstimage);
+	inHsvRange(srcimage, desc, dstimage);
 	morphOps(dstimage);
 
 	findContours(dstimage, contours, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE);
@@ -30,16 +30,22 @@ uint8_t locateFigure(figureDescription& desc, Mat& srcimage, Rect& rect)
 	for (uint32_t i = 0; i < contours.size(); i++) {
 
 		//  Find the area of contour
-		double a = contourArea(contours[i], false);
+		uint32_t a = (uint32_t)contourArea(contours[i], false);
 		if (a > largest_area) {
 
 			// Store the index of largest contour
 			largest_contour_index = i;
+			largest_area = a;
 		}
 	}
 
-	rect = boundingRect(contours[largest_contour_index]);
-	return contours.size() > 0;
+	if (contours.size() > 0) {
+
+		rect = boundingRect(contours[largest_contour_index]);
+		return 1;
+	}
+
+	return 0;
 }
 
 }
--- FigureDetection.h
+++ FigureDetection.h
@@ -2,24 +2,16 @@
  *
  * (c) domzigm 2016 - GPLv3
  * https://github.com/domzigm/mt
- * 
+ *
  */
-
+ 
 #pragma once
 
-#include <opencv2/opencv.hpp>
-#include <stdint.h>
+#include "Common.h"
 
 namespace mt
 {
 
-struct figureDescription
-{
-	cv::Scalar lowerThreshold;
-	cv::Scalar upperThreshold;
-};
-
-uint8_t locateFigure(figureDescription& desc, cv::Mat& srcimage, cv::Rect& rect);
+uint8_t locateFigure(const colorDescription& desc, const cv::Mat& srcimage, cv::Rect& rect);
 
 }
-#pragma once
